821ac4b1feba5667992d1c73e0fa2f4b9b1df1d6
Gotar <eltahir.g@gmail.com>
2021-02-03 04:59:09 -0600

    Fixed Gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -9,32 +9,32 @@ function createPlots(State) {
 
         // filter data by State and use the filter to extract need information to build charts
         let covidData = data.filter(item => item.State.toString() === State);
-        console.log(covidData);
+        // console.log(covidData);
 
         // create empy list and append states to it
         let states = [];
         covidData.forEach(item => states.push(item.State));
-        console.log(states);
+        // console.log(states);
 
         // append average cases to an empty list
         let averageCases = [];
         covidData.forEach(item => averageCases.push(item.Average_cases));
-        console.log(averageCases);
+        // console.log(averageCases);
 
         // append dates to an empty list
         let dates= [];
         covidData.forEach(item => dates.push(item.Date));
-        console.log(dates);
+        // console.log(dates);
 
         // append average deaths to an empty list
         let averageDeaths = [];
         covidData.forEach(item => averageDeaths.push(item.Average_death));
-        console.log(averageDeaths);
+        // console.log(averageDeaths);
 
         // append death percentage to an empty list
         let deathPercent = [];
         covidData.forEach(item => deathPercent.push(item.Death_percent));
-        console.log(deathPercent);
+        // console.log(deathPercent);
         
         // build horizontal bar chart to show monthly status
         let barTrace = {
@@ -58,102 +58,6 @@ function createPlots(State) {
         };
     
         Plotly.newPlot('bar', dataTrace, barLayout); 
-
-       // build gauge chart t show recovery percentage   
-        let filteredRecovery = covidData.filter(item => item.State.toString() === State);
-        console.log(filteredRecovery);
-
-        // append recovery percentage to an empty list
-        let recovery = [];
-        filteredRecovery.forEach(item => recovery.push(item.Recovery_percent));
-        console.log(recovery);
-
-        // Enter the recovery between 0 and 180
-        let level = parseFloat(recovery)*1.7;
-
-        // Trig to calc meter point
-        let degrees = 180 - level;
-        let radius = 0.5;
-        let radians = (degrees * Math.PI) / 180;
-        let x = radius * Math.cos(radians);
-        let y = radius * Math.sin(radians);
-
-        // Path: may have to change to create a better triangle
-        let mainPath = "M -.0 -0.05 L .0 0.05 L ";
-        let pathX = String(x);
-        let space = " ";
-        let pathY = String(y);
-        let pathEnd = " Z";
-        let path = mainPath.concat(pathX, space, pathY, pathEnd);
-        let gaugeData = [
-            {
-                type: "scatter",
-                x: [0],
-                y: [0],
-                marker: { size: 12, color: "850000" },
-                showlegend: false,
-                name: "Freq",
-                text: level,
-                hoverinfo: "text+name"
-            },
-            {
-                values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-                rotation: 90,
-                text: ["99-100", "98-99", "97-98", "96-97", "95-96", "94-95", "93-94", "92-93", "91-92", "90-91", ""],
-                textinfo: "text",
-                textposition: "inside",
-                marker: {
-                    colors: [
-                        "rgba(0, 100, 11, .5)",
-                        "rgba(0, 105, 11, .5)",
-                        "rgba(10, 120, 22, .5)",
-                        "rgba(14, 127, 0, .5)",
-                        "rgba(110, 154, 22, .5)",
-                        "rgba(170, 202, 42, .5)",
-                        "rgba(202, 209, 95, .5)",
-                        "rgba(210, 206, 145, .5)",
-                        "rgba(232, 226, 202, .5)",
-                        "rgba(240, 230, 215, .5)",
-                        "rgba(255, 255, 255, 0)"
-                    ]
-                },
-                labels: ["99-100", "98-99", "97-98", "96-97", "95-96", "94-95", "93-94", "92-93", "91-92", "90-91", ""],
-                hoverinfo: "label",
-                hole: 0.5,
-                type: "pie",
-                showlegend: false
-            }
-        ];
-
-        let gaugeLayout = {
-            shapes: [
-                {
-                    type: "path",
-                    path: path,
-                    fillcolor: "850000",
-                    line: {
-                        color: "850000"
-                    }
-                }
-            ],
-            title: "<b>Recovery Percentage</b> <br> Per Total Confirmed Cases",
-            height: 500,
-            width: 500,
-            xaxis: {
-                zeroline: false,
-                showticklabels: false,
-                showgrid: false,
-                range: [-1, 1]
-            },
-            yaxis: {
-                zeroline: false,
-                showticklabels: false,
-                showgrid: false,
-                range: [-1, 1]
-            }
-        };
-
-        Plotly.newPlot("gauge", gaugeData, gaugeLayout);
 
         // build line chart to show trend of average cases and death percentage during time period
        let lineTrace = {
@@ -181,32 +85,105 @@ function createPlots(State) {
     }).catch(err => console.log(err));
 };
 
-// function to display pandemic data information for selected State..
-function createData(State) {
+// build gauge chart to show recovery percentage for selcted State and Date
+function buildGauge(gauge) {
 
-    // API call
-    d3.json(url).then(data => {
-        let dataCopy1 = data;
+    // extract the recovery percent from list
+    let recovery = gauge.Recovery_percent;
+    console.log(recovery);
 
-        // filter by State and use filter to generate needed data
-        let filteredData = dataCopy1.filter(item => item.State.toString() === State)[12];
-        console.log(filteredData);
-        
-        // build pandemic data for selected state
-        let stateInfo = d3.select("#sample-metadata");
-        stateInfo.html("");
-        Object.entries(filteredData).forEach(key => {
-            stateInfo
-                .append("h5")
-                .text(key[0] + ": " + key[1])         
-        });
-    });
+    // Enter the recovery between 0 and 180 for the gauge needle has a 180 degrees movement
+    let level = parseFloat(recovery)*1.78;
+
+    // Trig to calc meter point
+    let degrees = 180 - level;
+    let radius = 0.5;
+    let radians = (degrees * Math.PI) / 180;
+    let x = radius * Math.cos(radians);
+    let y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    let mainPath = "M -.0 -0.05 L .0 0.05 L ";
+    let pathX = String(x);
+    let space = " ";
+    let pathY = String(y);
+    let pathEnd = " Z";
+    let path = mainPath.concat(pathX, space, pathY, pathEnd);
+    let gaugeData = [
+        {
+            type: "scatter",
+            x: [0],
+            y: [0],
+            marker: { size: 12, color: "850000" },
+            showlegend: false,
+            name: "Freq",
+            text: level,
+            hoverinfo: "text+name"
+        },
+        {
+            values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+            rotation: 90,
+            text: ["90-100", "80-90", "70-80", "60-70", "50-60", "40-50", "30-40", "20-30", "10-20", "0-10", ""],
+            textinfo: "text",
+            textposition: "inside",
+            marker: {
+                colors: [
+                    "rgba(0, 100, 11, .5)",
+                    "rgba(0, 105, 11, .5)",
+                    "rgba(10, 120, 22, .5)",
+                    "rgba(14, 127, 0, .5)",
+                    "rgba(110, 154, 22, .5)",
+                    "rgba(170, 202, 42, .5)",
+                    "rgba(202, 209, 95, .5)",
+                    "rgba(210, 206, 145, .5)",
+                    "rgba(232, 226, 202, .5)",
+                    "rgba(240, 230, 215, .5)",
+                    "rgba(255, 255, 255, 0)"
+                ]
+            },
+            labels: ["90-100", "80-90", "70-80", "60-70", "50-60", "40-50", "30-40", "20-30", "10-20", "0-10", ""],
+            hoverinfo: "label",
+            hole: 0.5,
+            type: "pie",
+            showlegend: false
+        }
+    ];
+
+    let gaugeLayout = {
+        shapes: [
+            {
+                type: "path",
+                path: path,
+                fillcolor: "850000",
+                line: {
+                    color: "850000"
+                }
+            }
+        ],
+        title: "<b>Recovery Percentage</b> <br> Per Total Confirmed Cases",
+        height: 500,
+        width: 500,
+        xaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        }           
+    };
+
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
 };
 
 // update function for state selection button..
 function optionChanged(State) {
     createPlots(State);
-    createData(State);
+    buildGauge(State);
 }
 
 // fuction to calculate unique characters in a string
@@ -216,34 +193,134 @@ function onlyUnique(value, index, self) {
 
 // intial function for updating charts based on State selected..
 function init() {
-    let stateIdButton = d3.select("#selDataset");
+    let stateIdButton = d3.select("#State");
 
     // API call
     d3.json(url).then(function(data) {
         let dataCopy2 = data;
-        console.log(dataCopy2);
+        // console.log(dataCopy2);
 
-        // append states in an smpty list
+        let filteredData7 = dataCopy2.filter(item => item.State);
+        console.log(filteredData7);
+
+        // append states in an empty list
         let states = [];
-        dataCopy2.forEach(item => states.push(item.State));
+        filteredData7.forEach(item => states.push(item.State));
 
         // select only unique state names
         let uniqueStates = states.filter((onlyUnique));
-        console.log(uniqueStates);
+        // console.log(uniqueStates);
 
         // update values based on selected state
         uniqueStates.forEach(name => {
+            // console.log(name)
         stateIdButton.append("option")         
             .text(name)        
             .property("value")
     
     });
         
-        // call functions to build charts and state data based on selected state
+        // call functions to build charts of selected State
         createPlots(uniqueStates[0]);
-        createData(uniqueStates[0]);
-    });
+
+        // initialize the demographic info and gauge chart
+        createData(filteredData7[0]);
+        buildGauge(filteredData7[0]);
+    }).catch(err => console.log(err));
+};
+// execute intial function
+init();
+
+// intial function for updating COVID-19 information based on Date selected..
+function demoInit() {
+    let dateIdButton = d3.select("#Date");
+    d3.json(url).then(function(data) {
+        let dataCopy3 = data;
+
+        // append dates in an smpty list
+        let dates = [];
+        dataCopy3.forEach(item => dates.push(item.Date));
+        // console.log(dates);
+
+        // select only unique dates
+        let uniqueDates = dates.filter((onlyUnique));
+        // console.log(uniqueDates)
+
+        // update values based on selected date
+        uniqueDates.forEach(date => {
+            dateIdButton
+                .append("option")         
+                .text(date)        
+                .property("value")
+        });
+    }).catch(err => console.log(err));     
+};
+// execute intial function
+demoInit();
+
+// here we are gonna build a filter to store State and Date from select buttons 
+// empty holder for filtered values
+let filters = {};
+// console.log(filters);
+
+// function to update filters list
+function updateFilters() {
+  // Save the element, value, and id of the filter that was changed
+  let changedElement = d3.select(this).select("select");
+  let elementValue = changedElement.property("value");
+  let filterId = changedElement.attr("id");
+
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  if (elementValue) {
+    filters[filterId] = elementValue;
+    console.log(filters)
+  }
+  else {
+    delete filters[filterId];
+  }
+
+  // Call function to apply all filters and rebuild the demographic info
+  filterDemo();
+}
+
+// function to apply all filters and rebuild the demographic info
+function filterDemo() {
+    d3.json(url).then(function(data) {
+        let dataCopy5 = data;
+
+        // Set the filteredDemoData
+        let filteredDemoData = dataCopy5.filter(item => item.State);
+        // console.log(filteredDemoData)
+
+        // Loop through all of the filters and keep any data that
+        // matches the filter values
+        Object.entries(filters).forEach(([key, value]) => {
+        filteredDemoData = filteredDemoData.filter(row => row[key] === value);
+        console.log(filteredDemoData)
+        });
+
+        // Finally, rebuild the demographic info and gauge using the filtered Data
+        createData(filteredDemoData[0]);
+        buildGauge(filteredDemoData[0]);
+    }).catch(err => console.log(err));
 };
 
-// execute initial function
-init();
+// function to create pandemic data information for selected State and Date..
+function createData(file) {
+    
+    // build pandemic data for selected filtered state and date
+    let stateInfo = d3.select("#sample-statedata");
+    stateInfo.html("");
+    Object.entries(file).forEach(key => {
+        stateInfo
+            .append("h5")
+            .text(key[0] + ": " + key[1])         
+    });
+
+};
+
+// Attach an event to listen for changes to each filter
+d3.selectAll(".well").on("change", updateFilters);
+
+
