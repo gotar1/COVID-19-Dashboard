7337e8329b1b75c3c84e92a6799ae388b4f3faa5
Gotar <eltahir.g@gmail.com>
2021-05-22 03:35:56 -0500

    Udated map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,101 +1,79 @@
 // Store our API endpoint inside queryUrl
 let url="/api/v1.0/states"
-console.log("hello")
+console.log("hello");
 
-// Perform a GET request to the query URL
-d3.json(url, function(data) {
-  console.log(data)
-//   // Once we get a response, send the data.features object to the createFeatures function
-//   // createFeatures(data.features);
-//   // console.log(data.features)
-});
+function buildPlot() {
 
-// function createMap(States) {
+  // Perform a GET request to the query URL
+  d3.json(url, function(data) {
+    console.log(data);
 
-//   // Define outdoormap layers
-//   let outdoormap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/outdoors-v11",
-//     accessToken: API_KEY
-//   });
+    let FilteredData = data.filter(item => item.State);
+    console.log(FilteredData);
 
-//   // Define darkmap layers
-//   let darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/dark-v10",
-//     accessToken: API_KEY
-//   });
+    let mapElement = {'state_name':[],
+                      'lat':[],
+                      'lon':[],
+                      'total_cases':[],
+                      'total_death':[]
+    };
+    FilteredData.forEach(item => {
+      mapElement.state_name.push(item.State),
+      mapElement.lat.push(item.Lat),
+      mapElement.lon.push(item.Long),
+      mapElement.total_cases.push(item.Total_cases),
+      mapElement.total_death.push(item.Total_deaths)
+    });
 
-//   // Define satellitemap layers
-//   let satellitemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/satellite-v9",
-//     accessToken: API_KEY
-//   });
+    let hoverText = mapElement['total_death'];
+    console.log(hoverText);
 
-//   // Define a baseMaps object to hold our base layers
-//   let baseMaps = {
-//     "outdoor Map": outdoormap,
-//     "Dark Map": darkmap,
-//     "satellite Map": satellitemap
-//   };
+    let mapData = [{
+      type: "choropleth",
+      locationmode: "USA-states",
+      locations: mapElement['state_name'],
+    //   geojson: mapElement['state_name'],
+    //   z: mapElement[cat],
+      lat: mapElement['lat'],
+      lon: mapElement['lon'],
+      color: mapElement['total_cases'],
+      text: hoverText,
+      hoverinfo: "text",
+      marker: {
+          size: 50,
+          line: {
+              color: "rgb(8,8,8)",
+              width: 1
+          },
+      }
+    }]
 
-//   // Create overlay object to hold our overlay layer
-//   let overlayMaps = {
-//     Covid: covids
-//   };
+    let layout = {
+      scope: "usa",
+      title: "USA COVID-19 Infection Map",
+      showlegend: false,
+      height: 600,
+            // width: 980,
+      geo: {
+        scope: "usa",
+        projection: {
+          type: "albers usa"
+        },
+        showland: true,
+        // landcolor: "rgb(217, 217, 217)",
+        subunitwidth: 1,
+        countrywidth: 1,
+        subunitcolor: "rgb(255,255,255)",
+        countrycolor: "rgb(255,255,255)"
+      }
+    };
 
-//   // Create our map, giving it the outdoormap and earthquakes layers to display on load
-//   let myMap = L.map("map", {
-//     center: [
-//       37.09, -95.71
-//     ],
-//     zoom: 5,
-//     layers: [outdoormap, covids]
-//   });
+    Plotly.newPlot("myMap", mapData, layout);
+  });
+}
 
-//   // Create a layer control
-//   // Pass in our baseMaps and overlayMaps
-//   // Add the layer control to the map
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
+buildPlot();
 
-//    // function to assign colors for legend markers
-//   function getColor(magnitude) {
-//     return magnitude > 5 ? '#f06b6b':
-//       magnitude > 4 ? '#f0936b':
-//       magnitude > 3 ? '#f3ba4e':
-//       magnitude > 2 ? '#f3db4c':
-//       magnitude > 1 ? '#e1f34c':
-//                       '#b7f34d';
-//   }
 
-//   // position legend on map bottim left corner
-//   let legend = L.control({position: 'bottomleft'});
 
-//   legend.onAdd = function() {
-//     let div = L.DomUtil.create('div', 'legend');
-//     let magnitudes = [0, 1, 2, 3, 4, 5];
-//     // legendLabels = [];
-//     div.innerHTML = 'Magnitude<br><hr>' 
-
-//     for (let i = 0; i < magnitudes.length; i++) {
-//       div.innerHTML +=
-//       '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i>' + magnitudes[i] + (magnitudes[i + 1] ?
-//       '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
-//     }
-//     return div
-//   };
-//   legend.addTo(myMap);
-// }
 
